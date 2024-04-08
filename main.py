cf640a75727bd11914a9f7c99d2d6eed3fb4cfd8
jokatty <jyotikattani@gmail.com>
2024-04-08 14:03:17 +0200

    add move turtle logic

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from turtle import Screen
 import time
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+# initiate player
+player = Player()
+
+# listen for the key press
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:

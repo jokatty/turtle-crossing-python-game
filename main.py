e3f50643a635fe785373a63a9f0f705b7f805ba1
jokatty <jyotikattani@gmail.com>
2024-04-08 15:56:24 +0200

    add collision logic

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ screen.listen()
 screen.onkey(player.move, "Up")
 
 # create car object
-car = CarManager()
+car_manager = CarManager()
 
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car.generate_car()
-    car.move_car()
+    car_manager.generate_car()
+    car_manager.move_car()
+    for car in car_manager.cars:
+        if player.distance(car) < 20:
+            game_is_on = False
 
 
 
